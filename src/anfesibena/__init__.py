5c0fd8d7770b449689010142e89a0d4c934592d0
Johannes <johannes-computer@mailfence.com>
2024-08-06 19:05:58 +0200

    Version 1.0.0

--- src/anfesibena/__init__.py
+++ src/anfesibena/__init__.py
@@ -1,6 +1,11 @@
-def main(args=None):
-    print("Hello World!")
-    
-if __name__ == '__main__':
-    main() 
-
+class Anfesibena:
+    def __div__(self, other):
+        return self * (other ** -1)
+    def __neg__(self):
+        return self * -1
+    def __rdiv__(self, other):
+        return other * (self ** -1)
+    def __rsub__(self, other):
+        return other + (self * -1)
+    def __sub__(self, other):
+        return self + (other * -1)
--- src/anfesibena/__main__.py
+++
@@ -1,5 +0,0 @@
-from anfesibena import main
-
-if __name__ == '__main__':
-    main()
-
